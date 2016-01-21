72c04fb14280bb2a029413b3585987d704320ddc
mwslater <mwslater@kent.edu>
2016-01-21 18:54:12 -0500

    Removed HTML code

--- src/hello.php
+++ src/hello.php
@@ -1,11 +1,1 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
-
-<body>
-<p><?php print('Hello, Code Louisville!') ?></p>
-</body>
-</html>
+<?php print('Hello, Code Louisville!') ?>
