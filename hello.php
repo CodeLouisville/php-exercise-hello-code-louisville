eb7d63f26954ae1947bc819371bc6c167235f10a
Tony Thigpen <tonytatsumaki@live.com>
2016-05-10 19:59:19 -0400

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,5 +1,9 @@
-<?php 
-
-echo "Hello, Code Louisville!";
-
-php?>
+<!DOCTYPE html>
+<html>
+  <head>
+  <title>Hello CL!</title>
+  </head>
+<body>
+<p><?php echo "Hello, Code Louisville!" ?></p>
+</body>
+</html>
