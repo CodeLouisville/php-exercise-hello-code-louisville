80d3a5f9d40488b42610993e0bd574b8e31b7191
Matthew <matthew.thompson.a@gmail.com>
2016-09-06 19:03:49 -0400

    removed h1 tag

--- hello.php
+++ hello.php
@@ -3,6 +3,6 @@
 // "Hello, Code Louisville!"
 //For this PHP code exercise, create a file named `hello.php` which simply outputs: `Hello, Code Louisville!`
 
-echo "<h1> Hello, Code Louisville!";
+echo "Hello, Code Louisville!";
 
 ?>
