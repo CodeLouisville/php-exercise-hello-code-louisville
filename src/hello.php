bcd6080347ecaf2c822fa3358c854e008d9cdbc8
jnels <s0ckeyeus@yahoo.com>
2016-09-06 19:05:43 -0400

    Undid formatting :(

--- src/hello.php
+++ src/hello.php
@@ -1,13 +1,3 @@
-<style>
-    h1 {
-        font-family: "Courier New";
-        font-size: 3em;
-        text-shadow: 1px 1px 2px #ccc;
-        text-align: center;
-        margin-top: 25px;
-    }
-</style>
-
 <?php
-    echo "<h1 class='blink'>Hello, Code Louisville!</h1>";
+    echo "Hello, Code Louisville!";
 ?>
