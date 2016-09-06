541b775f721f534fd741aeda18451a0af3dff891
lish06 <mlanth02@gmail.com>
2016-09-06 19:12:24 -0400

    Create hello.php

---
+++ hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <meta charset="utf-8">
+        <title>Hello| Louisville, KY</title>
+    </head>
+
+<?php
+
+echo "Hello, Code Louisville"
+
+?>
+
+</html>
