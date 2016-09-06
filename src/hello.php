90fe4d6104c391c1bea7fa1c138b099ef4131614
jnels <s0ckeyeus@yahoo.com>
2016-09-06 19:00:37 -0400

    Styled text

--- src/hello.php
+++ src/hello.php
@@ -1,3 +1,13 @@
+<style>
+    h1 {
+        font-family: "Courier New";
+        font-size: 3em;
+        text-shadow: 1px 1px 2px #ccc;
+        text-align: center;
+        margin-top: 25px;
+    }
+</style>
+
 <?php
-echo "Hello, Code Louisville!";
+    echo "<h1 class='blink'>Hello, Code Louisville!</h1>";
 ?>
